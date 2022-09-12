ca126cda503f1081c445249a4ac4a3ac48761cd0
Sudharshank1430 <112713453+Sudharshank1430@users.noreply.github.com>
2022-09-12 14:48:33 +0530

    Add files via upload

--- 9.py
+++ 9.py
@@ -1,12 +1,23 @@
-if (__name__=='__main__'):
-	T= int(input())
-	E = list(map,int,input(),split())
-	L= list(map,int,input(),split())
-	currentpresence = 0
-	maxpresence = 0 
-	for i in range(T):
-		currentpresence +=E[i]-L[i]
-	if (maxpresence < currentpresence):
-
-		maxpresence =currentpresence
-		print (maxpresence, end = '')
+E=[]
+L=[]
+t = int(input("T="))
+try:
+    if(t>0):
+        for i in range(t):
+            e=int(input("Guest entered="))
+            E.append(e)
+        for i in range(t):
+            l=int(input("Guest Leaved="))
+            L.append(l)
+        S=0
+        M=0
+        for i in range(t):
+            S+=E[i]-L[i]
+            M=max(S,M)
+        print("E=",E)
+        print("L=",L)
+        print("output=",M)
+    else:
+        print("INVALID T VALUE")
+except ValueError:
+    print("Enter all values ")
